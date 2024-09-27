215201028e0fc1232365475555519f9940d868df
Nicolás Vázquez <nikovazquez52@gmail.com>
2024-09-27 09:58:42 +0200

    ejercicio completo

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -4,15 +4,22 @@ import java.util.Stack;
 
 public class Exercise25 {
     public static Stack<String> createStack() {
-
+        Stack<String> stack = new Stack<>();
+        stack.push("Smith");
+        stack.push("Montessori");
+        stack.push("Peralta");
+        stack.push("House");
+        return stack;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
-
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
     }
 
     public static void main(String[] args) {
-
+        printAndEmptyStack(createStack());
     }
 
 
